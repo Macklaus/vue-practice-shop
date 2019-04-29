9a905ed1e4c61fd36b4bc67b33341786a7c40941
Juan Sebastian Cardona Morales <jcardona@internal.apexcorp.local>
2019-04-29 13:40:45 -0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="row">
+            <div :key="prod.id" class="col-md-6" v-for="prod in products">
+              <product v-on:add-to-cart="addToCart($event)" :product="prod" :isAlreadyInCart="productIsInCart(prod)"></product>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5 my-5">
+          <cart v-on:pay="pay()" v-on:remove-from-cart="removeFromCart($event)" :items="cart"></cart>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import products from '@/products.json';
+import Product from '@/components/Product.vue';
+import Cart from '@/components/Cart.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Product,
+    Cart
+  },
+  data(){
+    return {
+      products,
+      cart: []
+    }
+  },
+  methods: {
+    addToCart(event){
+      this.cart.push(event);
+    },
+    productIsInCart(product){
+      let item = this.cart.find(p => p.id === product.id);
+      if(item){
+        return true;
+      }
+      return false;
+    },
+    removeFromCart(event){
+      this.cart = this.cart.filter(product => product.id !== event.id);
+    },
+    pay(){
+      this.cart = [];
+      alert('Payment successfully');
+    }
   }
 }
 </script>
@@ -24,5 +64,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background-color: #FBF8F3;
 }
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <ul class="list-group">
+        <h1 class="text-center">Products added to cart</h1>
+        <li class="list-group-item" :key="item.id" v-for="item in items">
+            {{item.title}} - ${{ item.price }}
+            <button class="btn badge badge-danger float-rigth" @click="$emit('remove-from-cart', item)">Remove from cart</button>
+        </li>
+    </ul>
+    <div class="cart p-3 my-5">
+        <h4 class="text-center">${{total}}</h4>
+    </div>
+    <button :disabled="items.length === 0" @click="$emit('pay')" class="btn btn-info form-control">Pay Now</button>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['items'],
+    computed: {
+        total(){
+            return this.items.reduce((acc, item) => acc + Number(item.price), 0);
+        }
+    }
+}
+</script>
+
